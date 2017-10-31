ad1fc8c7e6fb855aff133cbe67e095a9f555d87e
[username] <yanxuegan0903@163.com>
2017-10-31 09:45:48 +0800

    Initial Commit

---
+++ XGVideoPlayer/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  XGVideoPlayer
+//
+//  Created by vsKing on 2017/10/31.
+//  Copyright © 2017年 vsKing. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
